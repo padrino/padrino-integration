088a2c5b6a7d40767d4619f3c16d6084a1936e02
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-05-13 23:20:41 +0200

    Make it really working in concurrency mode. Fix 1.9.2 issues.

--- spec/single_apps_spec.rb
+++ spec/single_apps_spec.rb
@@ -1,8 +1,8 @@
+# encoding: UTF-8
 require File.expand_path('../spec_helper.rb', __FILE__)
 
 describe "single-apps" do
-  before { kill_match("single-apps") }
-  after  { kill_match("single-apps") }
+  attr_reader :port, :app
 
   def app_path(app)
     "fixtures/single-apps/#{app}.rb"
@@ -13,106 +13,115 @@ describe "single-apps" do
   end
 
   def launch(app)
-    fork { `#{Gem.ruby} #{app_path(app)} #{get_free_port}` }
+    @app, @port  = app, get_free_port
+    fork { `#{Gem.ruby} #{app_path(app)} #{port}` }
     wait_localhost
   end
 
+  def kill
+    kill_match("#{app}.rb #{port}")
+  end
+
   describe "padrino_basic.rb" do
-    before { launch :padrino_basic }
+    before(:all) { launch :padrino_basic }
+    after(:all)  { kill }
 
     it "should get the original content" do
-      visit "http://localhost:3000"
+      visit "http://localhost:#{port}"
       body.should =~ /Im reloadable!/
     end
 
     it "should relaod inline content" do
       editing app_path(:padrino_basic), "Edited ...", "Edit ..." do
-        visit "http://localhost:3000"
+        visit "http://localhost:#{port}"
         body.should == "Edited ... Im reloadable!"
       end
     end
   end
 
   describe "padrino_advanced.rb" do
-    before { launch :padrino_advanced }
+    before(:all) { launch :padrino_advanced }
+    after(:all)  { kill }
 
     it "should get the original content" do
-      launch :padrino_advanced
-      visit "http://localhost:3000"
+      visit "http://localhost:#{port}"
       body.should == "Im\n\napp\nin a layout\n"
     end
 
     it "should reload the view" do
       editing view(:adv1), "Your", /^Im/ do
-        visit "http://localhost:3000"
+        visit "http://localhost:#{port}"
         body.should == "Your\n\napp\nin a layout\n"
       end
     end
 
     it "reload the layout" do
       editing view(:layout), "custom layout", /layout/ do
-        visit "http://localhost:3000"
+        visit "http://localhost:#{port}"
         body.should == "Im\n\napp\nin a custom layout\n"
       end
     end
   end
 
   describe "padrino_multi.rb" do
-    before { launch :padrino_multi }
+    before(:all) { launch :padrino_multi }
+    after(:all)  { kill }
 
     it "should get the original content" do
-      visit "http://localhost:3000"
+      visit "http://localhost:#{port}"
       body.should =~ /Given random/
       body[/(\d)/]
       random_was = $1
-      visit "http://localhost:3000"
+      visit "http://localhost:#{port}"
       body.should match(/Given random #{random_was}/)
       FileUtils.touch(app_path(:padrino_multi))
       sleep 2
-      visit "http://localhost:3000"
+      visit "http://localhost:#{port}"
       body.should_not =~ /^Given random #{random_was}$/
-      visit "http://localhost:3000/old"
+      visit "http://localhost:#{port}/old"
       body.should == "Old Sinatra Way"
-      visit "http://localhost:3000/2"
+      visit "http://localhost:#{port}/2"
       body.should == "The magick number is: 12!"
-      visit "http://localhost:3000/2/old"
+      visit "http://localhost:#{port}/2/old"
       body.should == "Old Sinatra Way"
     end
 
     it "should reload inline content" do
       editing app_path(:padrino_multi), "The magick number is: 14!", /The magick number is: 12!/ do
-        visit "http://localhost:3000/2"
+        visit "http://localhost:#{port}/2"
         body.should == "The magick number is: 14!"
       end
     end
   end
 
   describe "sinatra_rendering.rb" do
-    before { launch :sinatra_rendering }
+    before(:all) { launch :sinatra_rendering }
+    after(:all)  { kill }
 
     it "should get the original content" do
-      visit "http://localhost:3000"
+      visit "http://localhost:#{port}"
       body.should == "Basic text"
-      visit "http://localhost:3000/h1"
+      visit "http://localhost:#{port}/h1"
       body.should have_selector "h1", :content => "Only an h1 tag in haml"
-      pending "when the tilt patch will be relased" do
-        visit "http://localhost:3000/h1"
+      pending "waiting tilt haml utf8 patch" do
+        visit "http://localhost:#{port}/h1"
         body.should == "∴"
       end
     end
   end
 
   describe "sinatra_routing.rb" do
-    before { launch :sinatra_routing }
+    before(:all) { launch :sinatra_routing }
+    after(:all)  { kill }
 
     it "should get the original content" do
-      visit "http://localhost:3000"
+      visit "http://localhost:#{port}"
       body.should == "This is foo mapped as index"
-      visit "http://localhost:3000/bar"
+      visit "http://localhost:#{port}/bar"
       body.should == "Bar for html"
-      visit "http://localhost:3000/bar.js"
+      visit "http://localhost:#{port}/bar.js"
       body.should == "Bar for js"
-      visit "http://localhost:3000/custom-route/9"
+      visit "http://localhost:#{port}/custom-route/9"
       body.should == "This is a custom route with 9 as params[:id]"
     end
   end
